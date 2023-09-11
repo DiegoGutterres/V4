dbc3f0ca65ee34dee624ca016ecd15bafb08ff4c
Diego Gutterres <diegogfui123@gmail.com>
2023-09-10 23:52:26 -0300

    base 2

--- baixas.py
+++ baixas.py
@@ -6,6 +6,7 @@ import time
 
 #login automatico
 from config import CHROME_PROFILE_PATH
+control = 0
 
 #excel
 import pandas as pd
@@ -16,14 +17,62 @@ options.add_argument(CHROME_PROFILE_PATH)
 driver = webdriver.Chrome(options=options)
 driver.get("https://app.contaazul.com/#/financeiro/contas-a-receber?view=revenue&amp;source=Financeiro%20%3E%20Contas%20a%20Receber&source=Menu%20Principal")
 
-time.sleep(5)
+#ler a planilha
+document = pd.read_excel('info.xlsx')
+del document['Saldo Final']
+del document['Tipo do documento']
+del document['Código']
+del document['Referência']
+del document['E-mail']
+del document['Histórico']
+
 #inicio da automação
-data = driver.find_element(By.XPATH, '//*[@id="financeTopFilters"]/div[2]/button')
+time.sleep(30)
+#clicar pra abrir o filtro
+data = driver.find_element(By.XPATH, '//*[@id="financeTopFilters"]/div[2]')
 data.click()
 
+time.sleep(3)
+#filtrar por semana
 semana = driver.find_element(By.XPATH, '//*[@id="financeTopFilters"]/div[2]/ul/li[2]/a')
 semana.click()
+time.sleep(10)
 
+#tirar o filtro de recebido para diminuir o tanto de lançametos
+exibir = driver.find_element(By.XPATH, '//*[@id="type-filter-controller"]')
+exibir.click()
+time.sleep(2)
 
+recebido = driver.find_element(By.XPATH, '//*[@id="typeFilterContainer"]/li[4]/a/span[1]')
+recebido.click()
+time.sleep(2)
 
-time.sleep(30)
+aplicar = driver.find_element(By.XPATH, '//*[@id="type-filter"]/ul/li[2]/div/button')
+aplicar.click()
+time.sleep(5)
+
+#abrir todos lançamentos 
+while True:
+    try:
+        button = driver.find_element(By.XPATH, '//*[@id="conteudo"]/div/div[2]/div[2]/div/div[3]/button')
+        button.click()
+        time.sleep(10)
+    except:
+        break
+
+#pegar o nome dos clientes com base na planilha
+
+for each_client in document['Nome Cliente']:
+    # try:
+        print(each_client)  
+        
+        cliente = driver.find_element(By.XPATH, f'//span[contains(text(), "{each_client}")]') 
+        cliente.click()
+        time.sleep(10)
+    # except:
+    #     continue
+	
+
+    
+    
+    
--- config.py
+++ config.py
@@ -1,1 +1,2 @@
-CHROME_PROFILE_PATH = "user-data-dir=C:\\Users\\diego.gutterres_v4co\\AppData\\Local\\Google\\Chrome\\User Data\\ContaAzul"
+# CHROME_PROFILE_PATH = "user-data-dir=C:\\Users\\diego.gutterres_v4co\\AppData\\Local\\Google\\Chrome\\User Data\\ContaAzul"
+CHROME_PROFILE_PATH = "user-data-dir=C:\\Users\\T-GAMER\\AppData\\Local\\Google\\Chrome\\User Data\\ContaAzul"

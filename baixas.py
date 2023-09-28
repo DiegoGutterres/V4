e6502d3687d83adfccdf53c0a342ac779fd09646
DiegoGutterres <diegogfui123@gmail.com>
2023-09-28 10:44:32 -0300

    setup

--- baixas.py
+++ baixas.py
@@ -22,7 +22,12 @@ document.set_index('DATA')
 #driver
 options = webdriver.ChromeOptions()
 options.add_argument(CHROME_PROFILE_PATH)
-#options.add_argument('--headless') doideira
+options.add_experimental_option(
+    'prefs', {
+        "profile.managed_default_content_settings.images": 2,
+    }
+)
+#options.add_argument('--headless') #doideira
 driver = webdriver.Chrome(options=options)
 driver.get("https://app.contaazul.com/#/financeiro/contas-a-receber?view=revenue&amp;source=Financeiro%20%3E%20Contas%20a%20Receber&source=Menu%20Principal")
 
@@ -37,66 +42,60 @@ while True:
 
 #exibir
 time.sleep(2)
-exibir = driver.find_element(By.XPATH, '//*[@id="type-filter-controller"]/span')
-exibir.click()
+driver.find_element(By.XPATH, '//*[@id="type-filter-controller"]/span').click()
 time.sleep(2)
 
 #recebido
-recebido = driver.find_element(By.XPATH, '//*[@id="typeFilterContainer"]/li[4]/a/span[1]')
-recebido.click()
-time.sleep(1)
+driver.find_element(By.XPATH, '//*[@id="typeFilterContainer"]/li[4]/a/span[1]').click()
 
-aplicar = driver.find_element(By.XPATH, '//*[@id="type-filter"]/ul/li[2]/div/button')
-aplicar.click()
+#aplicar
+driver.find_element(By.XPATH, '//*[@id="type-filter"]/ul/li[2]/div/button').click()
 time.sleep(10)
 
 #filtrar contas
-contas = driver.find_element(By.XPATH, '//*[@id="bank-filter"]/button')
-contas.click()
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/button').click()
 
-all = driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/li[1]/a/span[1]')
-all.click()
+#all
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/li[1]/a/span[1]').click()
 time.sleep(1)
 
-bradesco = driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[2]/a/span')
-bradesco.click()
+#bradesco
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[2]/a/span').click()
 
-itau = driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[3]/a/span')
-itau.click()
+#itau
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[3]/a/span').click()
 
-sap = driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[4]/a/span')
-sap.click() 
+#sap
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[4]/a/span').click()
 
-sicredi = driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[5]/a/span')
-sicredi.click()
+#sicredi
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[5]/a/span').click()
 
-inadimplente = driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[10]/a/span')
-inadimplente.click()
+#inad
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[10]/a/span').click()
 
-franq_inad = driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[11]/a/span')
-franq_inad.click()
+#franq inad
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[11]/a/span').click()
 
-nova_data = driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[16]/a/span')
-nova_data.click()
+#nova data
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[16]/a/span').click()
 
-renovacao = driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[42]/a/span')
-renovacao.click() 
+#renovaçao
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/div/li[42]/a/span').click()
 
-aplicar_contas = driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/li[3]/div/button')
-aplicar_contas.click()
+#aplicar
+driver.find_element(By.XPATH, '//*[@id="bank-filter"]/ul/li[3]/div/button').click()
 time.sleep(10)
  
 #ir para cima
-up = driver.find_element(By.CSS_SELECTOR, 'body')
-up.send_keys(Keys.CONTROL, Keys.HOME)
+driver.find_element(By.CSS_SELECTOR, 'body').send_keys(Keys.CONTROL, Keys.HOME)
 time.sleep(2)
 
 #filtrar data
-data = driver.find_element(By.XPATH, '//*[@id="financeTopFilters"]/div[2]/button/span')
-data.click()
+driver.find_element(By.XPATH, '//*[@id="financeTopFilters"]/div[2]/button/span').click()
 
-todos = driver.find_element(By.XPATH, '//*[@id="financeTopFilters"]/div[2]/ul/li[5]/a')
-todos.click()
+#mostrar todos
+driver.find_element(By.XPATH, '//*[@id="financeTopFilters"]/div[2]/ul/li[5]/a').click()
 time.sleep(10)
 
 # --------- #
@@ -113,23 +112,24 @@ def func(cliente):
         print('sleeping')
         time.sleep(10)
 
-
     valor = driver.find_element(By.XPATH, '//*[@id="statement-list-container"]/table[1]/tbody/tr[1]/td[5]/div[2]')
     if (valor.text != document['VALOR'][cliente]):
         return    
 
-    abrir = driver.find_element(By.XPATH, '//*[@id="statement-list-container"]/table[1]/tbody/tr[1]/td[4]/div[1]/span[1]')
-    abrir.click()
+    #abrir
+    driver.find_element(By.XPATH, '//*[@id="statement-list-container"]/table[1]/tbody/tr[1]/td[4]/div[1]/span[1]').click()
     time.sleep(4)
 
     #conta azul funciona por id de conta, não pelo nome! (conta itau: 29329659)
     conta = driver.find_element(By.XPATH, '//*[@id="newIdConta"]')
     banco = driver.find_element(By.XPATH, '//*[@id="idBanco"]').get_attribute('value')
-    if banco != '29329659':
+    if banco != '29329698': #teste com a conta do bradesco
         conta.click()
         conta.send_keys(Keys.CONTROL, 'A', Keys.DELETE)
-        conta.send_keys('01.0 [Espelho Itaú] Receitas/Despesas FLUXO')
-        conta.send_keys(Keys.ENTER)
+        # conta.send_keys('01.0 [Espelho Itaú] Receitas/Despesas FLUXO')
+        # conta.send_keys(Keys.ENTER)
+        conta.send_keys('01.0 [Espelho Bradesco] Receitas/Despesas FLUXO')
+        time.sleep(2)
 
     today = date.today()
     data_formatada = today.strftime("%d%m%Y")
@@ -137,13 +137,14 @@ def func(cliente):
     data_atual.click()
     data_atual.send_keys(data_formatada)
 
-    btn_recebido = driver.find_element(By.XPATH, '//*[@id="formStatement"]/div[2]/div[2]/div[2]/label[4]/div/span[1]')
-    btn_recebido.click()
+    #recebido
+    driver.find_element(By.XPATH, '//*[@id="formStatement"]/div[2]/div[2]/div[2]/label[4]/div/span[1]').click()
     time.sleep(1)
 
-    final = driver.find_element(By.XPATH, '//*[@id="modal-footer-statement"]/div/button')
-    final.click()
+    #salvar
+    driver.find_element(By.XPATH, '//*[@id="finance-save-options"]/div[1]/button[2]').click()
     time.sleep(5)
+    print(f'feito cliente: {document["CLIENTE"][cliente]}')
 
 # --------- #
 
@@ -157,11 +158,14 @@ while True:
             print(f'transferencia de R${transferencia[i]}')
             cliente = i
             break
+        else: 
+            print('sem transferencia')
+            cliente = 0
     break
 
 for l in range(len(document['CLIENTE'])):
-    cliente += 1
     func(cliente)
+    cliente += 1
 
 
 
